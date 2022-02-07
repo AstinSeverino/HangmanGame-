f37107029c0efb32bc5cb31e823e4e09e3651242
astin027 <astin464@gmail.com>
2022-02-06 21:52:22 -0400

    primer commit de los programas de python

---
+++ list_practice.py
@@ -0,0 +1,18 @@
+def run ():
+    
+
+    # number = [i for i in range(1, 100000) if i % 36 == 0]
+
+    # for i in range (0, 100000):
+    #     if (i % 4 == 0 and i % 6 == 0 and i % 9 == 0):
+        # ansuer.append()
+        # if ansuer == i * 6:
+        #     elif == ansuer i * 9
+        #     continue:
+
+
+    print(number)
+
+
+if __name__=='__main__':
+    run()
