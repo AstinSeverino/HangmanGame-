6bcfa8dbe794d508b987ce1fa8589ab174e933b8
astin <astin464@gmail.com>
2022-02-11 17:07:47 -0400

    solucion de la prueba de verificar los numbreros si son divisible

--- list_practice.py
+++ list_practice.py
@@ -1,18 +1,22 @@
 def run ():
+    valos = []   
     
 
-    number = [i for i in range(1, 100000) if i % 36 == 0]
+    # # number = [i for i in range(1, 100000) if i % 36 == 0]
 
-    for i in range (0, 100000):
-        if (i % 4 == 0 and i % 6 == 0 and i % 9 == 0):
-        ansuer.append()
-        if ansuer == i * 6:
-            elif == ansuer i * 9
-            continue:
+    # for i in range (1, 100000):
+    #     if i % 36 != 0:
+    #         return i
+    #     print(valos)
+    #     break
+             
 
-
-    print(number)
-
+    # # print(number)
+    for i in range (0, 10000):
+        if i % 36==0:
+            print(i) 
+#primer resultado de la vuelta al uso de python 
+#aun nose usar returm 
 
 if __name__=='__main__':
     run()
