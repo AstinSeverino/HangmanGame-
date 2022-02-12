3afa4ca64976c03e10e4489d90928a5f20857fdc
astin <astin464@gmail.com>
2022-02-12 07:38:48 -0400

    praticando lambda funtions in python aun nose como se imprime el resultado de la operacion  :>

---
+++ lambdaFuncion.py
@@ -0,0 +1,11 @@
+from re import X
+
+
+def run():
+    pass
+square=int(input("ingresa un numero _:"))
+square=lambda x: x%2==0
+print("es numero es divisible entro 2")
+
+if __name__=='__main__ ':
+    run()
