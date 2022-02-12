5da263cb2082639e20f2deace8a370c174aa9012
astin <astin464@gmail.com>
2022-02-12 07:46:14 -0400

    las lamdba funtion no tieme que imprimir cosas como def asi, la documentacion del codigo esta

--- lambdaFuncion.py
+++ lambdaFuncion.py
@@ -6,6 +6,8 @@ def run():
 square=int(input("ingresa un numero _:"))
 square=lambda x: x%2==0
 print("es numero es divisible entro 2")
+#las funciones anonimas son para casos donde no se quiere usar una funcion
+# con def. y se usa con las expresions de high orden funtion con map reduce and filter
 
 if __name__=='__main__ ':
     run()
