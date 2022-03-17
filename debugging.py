4fb0b5bfe714dd2519e9fe65352117832eb207cc
astin <astin464@gmail.com>
2022-03-17 15:47:05 -0400

    raise exception con debugging para comprobar si el numero es negativo

--- debugging.py
+++ debugging.py
@@ -7,11 +7,16 @@ def divisor (num1):
         if num1 % i ==1:
             divisor.append(i)
     return divisor
+    
 def run ():
-    num1= int(input('ingrese su numero para saber la multi'))
-    print(divisor(num1))
-    print('bye my friend')
-
+    try:
+        num1= int(input('ingrese su numero para saber la multi'))
+        if num1 <= 0:
+            raise Exception('Negative number is not valid')
+        print(divisor(num1))
+        print('bye my friend')
+    except ValueError:
+        print("debe se un numero")
 
 
 if __name__=='__main__':
