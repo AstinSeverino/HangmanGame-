458bc65a01a61f30c8be6457cd3b6f3ede1968ff
astin <astin464@gmail.com>
2022-03-20 06:06:29 -0400

    obteniedo los datos de los dicionarios iniciando proyecto

---
+++ hangmanGame/hangmanGame.py
@@ -0,0 +1,20 @@
+def read():
+    words=[]
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        return f 
+        #utd-8 es para corregir el texto en espanol y na tilde y eso    
+    #     for line in f:
+    #         # f.w("\n")
+    #         words.append(line) 
+    # print(words)        
+
+def proData():
+    box =[]
+    add 
+
+def run ():
+    pass
+
+
+if __name__=="__main__":
+    read()
