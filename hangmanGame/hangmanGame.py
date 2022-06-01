48d84eca42a3c852b4927c8cdba000c855902540
astin <astin464@gmail.com>
2022-06-01 17:02:16 -0400

    todo funciona compara los las palabras de la lista pero tengo un bug que no deja escribir el ultimo slide por que no se cual se esta agredando

--- hangmanGame/hangmanGame.py
+++ hangmanGame/hangmanGame.py
@@ -7,31 +7,33 @@ from random import*
 print("\nC O M I E N Z A  E L J U E G O \n A D I V I N A  L A  L E T R A\n\n")
 with open("./archivos/data.txt", "r", encoding="utf-8") as f:
     #abre el archivo y lo pone en una variable es esta caso f 
-    dataList=dict(enumerate(f,1))
+    dataList=list(f)
     #crea un dicionario(Tambien podria ser lista o tubla si ponemos el nombre)y lo enemura con enumerate 
     #y despues la variable y el numero donde arranca
-    realDataList=dataList
     # print(real)
-    valor=Random().choice(realDataList)
+    valor=Random().choice(dataList)
      #random().choice(nombre de la variable aqui) necesita los parentisis para poder funcionar por que es una clase        
      #esto seleciona random algo lista letra plabra etc
     print(valor)
-    valor=dict(enumerate(valor))
-    print(valor)
-    verificar= input("INTRUCE LA LETRA ")# me quede probando con el metodo get para ver si puede verificar si las letra que entra el usuario estan ahi 
-    # print(valor.get("verificar","El valor no se encuentra en el dic"))
-    for i in valor.get(i):
-        if i == verificar:
-            print("som igaules")
-        else:
-            print("no so iguales")
-        # if valor == verificar: 
-            # print(valor.get(i,"no esta"))
-    # print(valor)
-    # print(len(valor)) 
-    # print(" _ " * len(valor))
+    display=[]
+    wordLen=len(valor)-1
+    for letter in valor:
+        display += "_"
+    print(display)
 
-    
+    while '_' in display:
+        guessWord= input("Adivina la palabra").lower()
+
+        for i in range(wordLen):
+            word= valor[i]
+            if word == guessWord:
+                display[i]=word
+        print(display)
+        print(valor)
+        if not "_" in display:
+            print("you wow u are fanstastic")
+        
+        
 
    
           
