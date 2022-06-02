fc18aa148219a16ab01d858fc9bc2445a08b50ea
astin <astin464@gmail.com>
2022-06-02 10:52:45 -0400

    El juego funciona solo tenemos que arreglar el tema de las mayusculas para que el mismo las cambie y poder verificar otras palabras

--- hangmanGame/hangmanGame.py
+++ hangmanGame/hangmanGame.py
@@ -1,35 +1,46 @@
 from multiprocessing import Value
 from optparse import Values
 from random import*
+import random
 
 
 
 print("\nC O M I E N Z A  E L J U E G O \n A D I V I N A  L A  L E T R A\n\n")
 with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+    words=[]
+    for line in f:
+        words.append(line.strip().upper())
     #abre el archivo y lo pone en una variable es esta caso f 
-    dataList=list(f)
+    # dataList=list(f)
+    # print(dataList)
     #crea un dicionario(Tambien podria ser lista o tubla si ponemos el nombre)y lo enemura con enumerate 
     #y despues la variable y el numero donde arranca
     # print(real)
-    valor=Random().choice(dataList)
+    valor=random.choice(words)
+    # valor.strip().upper()
      #random().choice(nombre de la variable aqui) necesita los parentisis para poder funcionar por que es una clase        
      #esto seleciona random algo lista letra plabra etc
     print(valor)
+    #para quitar los acentos a las palabras 
+    new_sentence = valor.maketrans('áéíóú', 'aeiou')
+    n_valor = valor.translate(new_sentence)
+    print(n_valor)
     display=[]
-    wordLen=len(valor)-1
-    for letter in valor:
+    wordLen=len(n_valor)
+    print(wordLen)
+    for letter in n_valor:
         display += "_"
     print(display)
 
-    while '_' in display:
-        guessWord= input("Adivina la palabra").lower()
+    while "_" in display:
+        guessWord= input("Adivina la palabra \n").upper()
 
         for i in range(wordLen):
-            word= valor[i]
+            word= n_valor[i]
             if word == guessWord:
                 display[i]=word
         print(display)
-        print(valor)
+        print(n_valor)
         if not "_" in display:
             print("you wow u are fanstastic")
         
