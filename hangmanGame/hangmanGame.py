95375754a8d596efc7637439724465e81fe2762a
astin <astin464@gmail.com>
2022-06-08 10:03:03 -0400

    solucionando el bug de los acentos y el input

--- hangmanGame/hangmanGame.py
+++ hangmanGame/hangmanGame.py
@@ -7,25 +7,28 @@ import random
 
 print("\nC O M I E N Z A  E L J U E G O \n A D I V I N A  L A  L E T R A\n\n")
 with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+    dataList=list(f)
+    print()
     words=[]
-    for line in f:
+    for line in dataList:
         words.append(line.strip().upper())
     #abre el archivo y lo pone en una variable es esta caso f 
-    # dataList=list(f)
+    
     # print(dataList)
     #crea un dicionario(Tambien podria ser lista o tubla si ponemos el nombre)y lo enemura con enumerate 
     #y despues la variable y el numero donde arranca
     # print(real)
-    valor=random.choice(words)
+    valor=random.choice(dataList)
     # valor.strip().upper()
      #random().choice(nombre de la variable aqui) necesita los parentisis para poder funcionar por que es una clase        
      #esto seleciona random algo lista letra plabra etc
     print(valor)
-    #para quitar los acentos a las palabras 
-    # new_sentence = valor.maketrans('áéíóú', 'aeiou')
-    # n_valor = valor.translate(new_sentence)
-    # print(n_valor)
-    n_valor= valor
+    # para quitar los acentos a las palabras 
+    valor2="".join(valor)
+    print(valor2)
+    new_sentence = valor2.maketrans('áéíóú', 'aeiou')
+    n_valor = valor2.translate(new_sentence)
+    print(n_valor)
     display=[]
     wordLen=len(n_valor)
     print(wordLen)
