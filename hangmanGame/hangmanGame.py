6173f3f3fa246038104a231f0bd9192812192f8f
astin <astin464@gmail.com>
2022-06-13 10:19:56 -0400

    por fin puede quitar el salto de linea y el ultimo valor de la lista solo queda ponerlo para que use esa liena

--- hangmanGame/hangmanGame.py
+++ hangmanGame/hangmanGame.py
@@ -8,7 +8,8 @@ import random
 print("\nC O M I E N Z A  E L J U E G O \n A D I V I N A  L A  L E T R A\n\n")
 with open("./archivos/data.txt", "r", encoding="utf-8") as f:
     dataList=list(f)
-    print()
+
+    print(dataList)
     # words=[]
     # for line in dataList:
     #     words.append(line.strip().upper())
@@ -19,21 +20,30 @@ with open("./archivos/data.txt", "r", encoding="utf-8") as f:
     #y despues la variable y el numero donde arranca
     # print(real)
     valor=random.choice(dataList)
+    lisTen=[]
+    for i in valor:
+        lisTen.append(i.replace("\n", ""))
+    lisTen.pop(-1) 
+    print(lisTen)
+    print(len(lisTen))
     # valor.strip().upper()
      #random().choice(nombre de la variable aqui) necesita los parentisis para poder funcionar por que es una clase        
      #esto seleciona random algo lista letra plabra etc
-    print(valor)
     # para quitar los acentos a las palabras 
     valor2="".join(valor)
     print(valor2)
     new_sentence = valor2.maketrans('áéíóú', 'aeiou')
     n_valor = valor2.translate(new_sentence)
     print(n_valor)
+
     display=[]
+
     
     words=[]
-    for line in n_valor:
+    for line in n_valor: 
         words.append(line.strip().upper())
+    
+        
 
     wordLen=len(words)
     print(wordLen)
